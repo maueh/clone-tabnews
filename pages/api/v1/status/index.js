4dd960716e1d38dfe66a7da0f1333245e28b33eb
Marcel <uehdev@gmail.com>
2024-10-14 22:35:11 -0300

    finaliza endpoint de  `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,34 +3,44 @@ import database from "infra/database";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
-  const updateAt2 = Date.now();
-
-  //Connecting do the databse
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
+  // const updateAt2 = Date.now();
 
   //Preparing the values to return
-  const dbVersion = await database.query(
-    "SELECT current_setting('server_version') AS version;",
+  // "SELECT version();" dbVersion.rows[0].version
+  // "SELECT current_setting('server_version') AS version;" dbVersion.rows[0].version
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
   );
-  const maxConnections = await database.query("SHOW max_connections;");
-  const usedConnections = await database.query(
-    "SELECT COUNT(*) FROM pg_stat_activity;",
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
   );
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  //console.log(databaseOpenedConnectionsResult);
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  //console.log("max: " + databaseMaxConnectionsValue);
+  //console.log("Using: " + databaseOpenedConnectionsValue);
 
   response.status(200).json({
     update_at: updateAt,
-    update2: updateAt2,
-    database_version: dbVersion.rows[0].version,
-    max_connections: maxConnections.rows[0].max_connections,
-    used_connections: usedConnections.rows[0].count,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
-  await client.end();
+  //await client.end();
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,20 @@
+test.only("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+});
+
+/*
 describe("database.js tests", () => {
   test("GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
@@ -14,38 +31,48 @@ describe("database.js tests", () => {
     const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
     expect(responseBody.update_at).toEqual(parsedUpdateAt);
   });
+});
 
-  //postgres version
-  describe("Checking Postgres Version", () => {
-    test("dbVersion should be defined", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
-      expect(responseBody.database_version).toBeDefined();
-    });
-  });
-
-  //maximumConnections
-  describe("Checking maximum connections", () => {
-    test("max_connections should be defined", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
-      expect(responseBody.max_connections).toBeDefined();
-      expect(parseInt(responseBody.max_connections)).toBeGreaterThanOrEqual(0);
-      expect(parseInt(responseBody.max_connections)).toBeLessThanOrEqual(100);
-    });
-  });
-
-  //usedConnections
-  describe("Checking used_connections", () => {
-    test("max_connections should be defined", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
-      console.log("m: " + responseBody.used_connections);
-      expect(responseBody.used_connections).toBeDefined();
-      expect(parseInt(responseBody.used_connections)).toBeGreaterThanOrEqual(0);
-      expect(parseInt(responseBody.used_connections)).toBeLessThanOrEqual(
-        parseInt(responseBody.max_connections),
-      );
-    });
+//postgres version
+describe("Checking Postgres Version", () => {
+  test("dbVersion should be defined", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.version).toBeDefined();
+    expect(responseBody.dependencies.database.version).toBe("16.0");
   });
 });
+
+//maximumConnections
+describe("Checking maximum connections", () => {
+  test("max_connections should be defined", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.max_connections).toBeDefined();
+    expect(
+      responseBody.dependencies.database.max_connections,
+    ).toBeGreaterThanOrEqual(0);
+    expect(
+      responseBody.dependencies.database.max_connections,
+    ).toBeLessThanOrEqual(100);
+  });
+});
+
+//usedConnections
+describe("Checking used_connections", () => {
+  test("max_connections should be defined", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
+    console.log("m: " + responseBody.dependencies.database.used_connections);
+    expect(responseBody.dependencies.database.used_connections).toBeDefined();
+    expect(
+      parseInt(responseBody.dependencies.database.used_connections),
+    ).toBeGreaterThanOrEqual(0);
+    expect(
+      parseInt(responseBody.dependencies.database.used_connections),
+    ).toBeLessThanOrEqual(
+      parseInt(responseBody.dependencies.database.max_connections),
+    );
+  });
+});
+*/
